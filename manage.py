82b8c49b09d1a55727d1bc45f2197719243fc81b
daremasac <darema.tech@gmail.com>
2026-02-06 23:58:18 -0500

    levantando observaciones

--- manage.py
+++ manage.py
@@ -6,18 +6,19 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'encuesta.settings')
+   
 
     # --- PARCHE PARA MYSQL (PyMySQL) ---
-    # try:
-    #     import pymysql
-    #     pymysql.install_as_MySQLdb()
-    #     import MySQLdb
-    #     # Engañamos a Django con la versión requerida
-    #     MySQLdb.version_info = (2, 2, 1, "final", 0)
-    # except ImportError:
-    #     pass
-
+    try:
+        import pymysql
+        pymysql.install_as_MySQLdb()
+        import MySQLdb
+        # Engañamos a Django con la versión requerida
+        MySQLdb.version_info = (2, 2, 1, "final", 0)
+    except ImportError:
+        pass
+    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'encuesta.settings')
     
     try:
         from django.core.management import execute_from_command_line
